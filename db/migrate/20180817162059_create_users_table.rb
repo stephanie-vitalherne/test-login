ac4ef048bc569227e4853e1d9b1489cef31fcdf8
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-17 14:04:59 -0400

    more

--- db/migrate/20180817162059_create_users_table.rb
+++ db/migrate/20180817162059_create_users_table.rb
@@ -1,9 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :email
       t.string :first_name
       t.string :password
-      t.string :email
       t.datetime :created_at
       t.datetime :updated_at
     end
