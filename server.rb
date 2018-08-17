e771a9cc4fd28cf6d126643fedcca202aaeed436
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-17 14:40:01 -0400

    created redirect and get route for account after you enter data into the form and it grabs it

--- server.rb
+++ server.rb
@@ -20,6 +20,12 @@ post '/login' do
     first_name: params['first_name'],
     email: params['email']
   )
+  p "Welcome to the site, #{params['first_name']}"
+  redirect :account
+end
+
+get '/account' do
+  erb :account
 end
 
 require './models'
